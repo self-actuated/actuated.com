71e4078423789517b4884ec654422bc8dd02b268
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2022-12-09 14:08:28 +0000

    Update favicon
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import {
   Bars3Icon,
-  ChatBubbleBottomCenterTextIcon,
-  ChatBubbleLeftRightIcon,
-  QuestionMarkCircleIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
